382bd5348bbcaf58455de88c30abeb6b27063dce
Peter Seibel <peter@gigamonkeys.com>
2015-05-24 21:27:36 -0700

    Add stats output and clean up data file at end.

--- quiz.hs
+++ quiz.hs
@@ -1,6 +1,8 @@
+import Control.Concurrent
 import Data.List
 import Data.Maybe
 import System.Console.ANSI
+import System.Directory
 import System.IO
 import System.Posix.Files
 import System.Random
@@ -31,7 +33,9 @@ instance Question MathQuestion where
     answer     = show . eval
     check q r  = read r == eval q
 
-timesTable max = [ MathQuestion Mult x y | x <- [0..max], y <- [0..max] ]
+timesTable n max = [ MathQuestion Mult n x | x <- [0..max] ] ++ [ MathQuestion Mult x n | x <- [0..max] ]
+
+--timesTable max = [ MathQuestion Mult x y | x <- [0..max], y <- [0..max] ]
 
 -- Text questions
 
@@ -54,54 +58,63 @@ data Quiz q = Quiz { count :: Int
                    , asked :: H.MinPrioHeap Int (ToAsk q)
                    , unasked :: [q]
                    , limit :: Int
+                   , retired :: Int
                    } deriving (Show, Read)
 
 newToAsk q = ToAsk q 1 0
 
 toAsk :: Quiz q -> Maybe (ToAsk q, Quiz q)
-toAsk (Quiz _ asked [] _) | H.isEmpty asked= Nothing
-toAsk (Quiz count asked [] limit) = H.view asked >>= (\((_, a), rest) -> return (a, (Quiz count rest [] limit)))
-toAsk (Quiz count asked (q:qs) limit) | H.isEmpty asked = Just (newToAsk q, Quiz count asked qs limit)
-toAsk (Quiz count asked (q:qs) limit) = Just (if c <= count then oldQuestion else newQuestion) where
+toAsk (Quiz _ asked [] _ _) | H.isEmpty asked= Nothing
+toAsk (Quiz count asked [] limit retired) = H.view asked >>= (\((_, a), rest) -> return (a, (Quiz count rest [] limit retired)))
+toAsk (Quiz count asked (q:qs) limit retired) | H.isEmpty asked = Just (newToAsk q, Quiz count asked qs limit retired)
+toAsk (Quiz count asked (q:qs) limit retired) = Just (if c <= count then oldQuestion else newQuestion) where
     ((c, a), rest) = fromJust $ H.view asked
-    newQuestion = (newToAsk q, Quiz count asked qs limit)
-    oldQuestion = (a, Quiz count rest (q:qs) limit)
+    newQuestion = (newToAsk q, Quiz count asked qs limit retired)
+    oldQuestion = (a, Quiz count rest (q:qs) limit retired)
 
 -- Take the question just asked and whether it was answered correctly and put it back into the Quiz.
 answered :: (ToAsk q) -> Bool -> (Quiz q) -> (Quiz q)
-answered (ToAsk q gap correct) ok (Quiz count asked qs limit) = Quiz (count + 1) newAsked qs limit where
+answered (ToAsk q gap correct) ok (Quiz count asked qs limit retired) = Quiz (count + 1) newAsked qs limit newRetired where
     ask        = ToAsk q newGap newCorrect
     newAsked   = if newCorrect == limit then asked else H.insert (count + newGap, ask) asked
     newGap     = if ok then gap * 2 else 1
     newCorrect = if ok then correct + 1 else 0
+    newRetired = if newCorrect == limit then retired + 1 else retired
 
-quiz qs = Quiz 0 H.empty qs 10
+quiz qs = Quiz 0 H.empty qs 2 0
 
 getAnswer :: IO Int
 getAnswer = readLn
 
 askIt :: (Question q) => q -> IO Bool
 askIt q = do
-  clearScreen
   putStr $ question q
   hFlush stdout
   r <- getLine
   let ok = check q r
   putStrLn $ if ok then "Right!" else "Oops. The answer is " ++ (answer q) ++ "."
+  threadDelay 300000
   _ <- if not ok then getLine else return ""
   return ok
 
-foo :: (Question q, Show q) => Maybe (ToAsk q, Quiz q) -> IO ()
-foo (Just (ToAsk q gap correct, qz)) = do
-  ok <- askIt q
-  let qz' = answered (ToAsk q gap correct) ok qz
-  saveQuiz qz' "quiz.dat"
-  runQuiz qz'
 
-foo Nothing = putStrLn "Looks like you've got them all."
+stats (Quiz count asked unasked limit retired) = "done: " ++ (show retired) ++ "; in play: " ++ (show $ H.size asked) ++ "; unasked: " ++ (show $ length unasked) ++ "; limit: " ++ (show limit)
 
 runQuiz :: (Question q, Show q) => Quiz q -> IO ()
-runQuiz quiz = foo (toAsk quiz)
+runQuiz quiz = run (toAsk quiz) where
+    run (Just (ToAsk q gap correct, qz)) = do
+      clearScreen
+      putStrLn $ stats qz
+      ok <- askIt q
+      let qz' = answered (ToAsk q gap correct) ok qz
+      saveQuiz qz' "quiz.dat"
+      runQuiz qz'
+
+    run Nothing = do
+      removeFile "quiz.dat"
+      putStrLn "Looks like you've got them all."
+
+
 
 mathQuestions = [ MathQuestion Plus a b | a <- [0..10], b <- [0..10] ]
 
@@ -133,10 +146,10 @@ saveQuiz q p = writeFile p (show q)
 loadQuiz :: FilePath -> IO (Quiz MathQuestion)
 loadQuiz p = readFile p >>= (\s -> readIO s)
 
-newQuiz = getStdGen >>= (\rng -> return $ quiz $ shuffledQuestions (timesTable 20) rng)
 
+newQuiz n = getStdGen >>= (\rng -> return $ quiz $ shuffledQuestions (timesTable n 20) rng)
 
 main = do
   exists <- fileExist "quiz.dat"
-  q <- if exists then loadQuiz "quiz.dat" else newQuiz
+  q <- if exists then loadQuiz "quiz.dat" else readLn >>= newQuiz
   runQuiz q
